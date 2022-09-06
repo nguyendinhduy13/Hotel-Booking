c25517ba88e74ef7821cb92a8239a6648d28fff5
Huy <Huy@1234>
2022-09-06 10:34:39 +0700

    design home

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import "react-native-gesture-handler"
-import {View} from "react-native"
+import { View } from "react-native"
 import { StatusBar } from 'react-native'
 import COLORS from './src/consts/colors'
 import RootNavigation from './src/view/navigation/RootNavigation'
 import { SignInContextProvider } from './src/contexts/authContext'
-
-
-export default function App(){
-  return(
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
+console.disableYellowBox = true;
+export default function App() {
+  return (
     <SignInContextProvider>
-    <View style={{flex:1}}>
-      <StatusBar backgroundColor={COLORS.white} barStyle="dark-content"/>
-      <RootNavigation/>
-    </View>
+      <View style={{ flex: 1 }}>
+        <StatusBar backgroundColor={COLORS.white} barStyle="dark-content" />
+        <RootNavigation />
+      </View>
     </SignInContextProvider>
   )
 }
--- src/view/screens/HotelPhotos.js
+++ src/view/screens/HotelPhotos.js
@@ -1,75 +1,77 @@
 import React, { useEffect, useState } from "react";
-import {View,Text,Image, TouchableOpacity, Modal, ScrollView} from "react-native"
+import { View, Text, Image, TouchableOpacity, Modal, ScrollView } from "react-native"
 import { FlatList } from "react-native-gesture-handler";
 import Icon from "react-native-vector-icons/AntDesign"
 import COLORS from "../../consts/colors";
 import hotels from "../../consts/hotels";
 
-export default function HotelPhotos({route,navigation}){
-        const [item,setItem]=useState(route.params)
-        const [index,setIndex]=useState(0)
-        const [itemzoom,setitemzoom]=useState([])
-        const [modalVisible,setModalVisible]=useState(false)
-        const ListImage=({hotel})=>{
-              if(hotel.id==item.id){
-                return (
-                        <View>
-                                {hotel.image.map((image,index)=>(
-                                <TouchableOpacity onPress={()=>{setModalVisible(!modalVisible),setitemzoom(item),setIndex(index)}}> 
-                                <Image
-                                 source={{uri:image}}
-                                 style={{width:160,height:150,marginTop:20,marginHorizontal:15,borderRadius:20}}
-                                /> 
-                                </TouchableOpacity>
-                                 ))}
-                        </View>
-                )
+export default function HotelPhotos({ route, navigation }) {
+        const [item, setItem] = useState(route.params)
+        const [index, setIndex] = useState(0)
+        const [itemzoom, setitemzoom] = useState([])
+        const [modalVisible, setModalVisible] = useState(false)
+        const ListImage = ({ hotel }) => {
+                if (hotel.id == item.id) {
+                        return (
+                                <View>
+                                        {hotel.image.map((image, index) => (
+                                                <TouchableOpacity onPress={() => { setModalVisible(!modalVisible), setitemzoom(item), setIndex(index) }}>
+                                                        <Image
+                                                                source={{ uri: image }}
+                                                                style={{ width: 160, height: 150, marginTop: 20, marginHorizontal: 15, borderRadius: 20 }}
+                                                        />
+                                                </TouchableOpacity>
+                                        ))}
+                                </View>
+                        )
+                }
         }
-}
-        
 
-        return(
-                <ScrollView contentContainerStyle={!modalVisible?{
+
+        return (
+                <ScrollView contentContainerStyle={!modalVisible ? {
                         backgroundColor: COLORS.white,
-                        flex:1,
-                        paddingBottom:20,
-                }:{backgroundColor: COLORS.grey,
-                        flex:1,
-                        paddingBottom:20,}}>
-                        <View style={{paddingTop:"10%",marginHorizontal:15,flexDirection:"row"}}>
+                        flex: 1,
+                        paddingBottom: 20,
+                } : {
+                        backgroundColor: COLORS.grey,
+                        flex: 1,
+                        paddingBottom: 20,
+                }}>
+                        <View style={{ paddingTop: "10%", marginHorizontal: 15, flexDirection: "row" }}>
                                 <Icon
-                                 name="arrowleft"
-                                 size={30}
-                                 onPress={()=>navigation.goBack()}
-                                />  
-                                <Text style={{fontSize:19,fontWeight:"bold",color:COLORS.black,marginHorizontal:20,marginTop:2}}>
+                                        name="arrowleft"
+                                        size={30}
+                                        onPress={() => navigation.goBack()}
+                                />
+                                <Text style={{ fontSize: 19, fontWeight: "bold", color: COLORS.black, marginHorizontal: 20, marginTop: 2 }}>
                                         Gallery Hotel Photos
                                 </Text>
-                        </View>  
-                        
+                        </View>
+
                         <View>
                                 <FlatList
-                                data={!modalVisible?hotels:""}
-                                horizontal={false}
-                                showsHorizontalScrollIndicator={false}
-                                numColumns={2}
-                                renderItem={({ item }) => <ListImage hotel={item} />}
+                                        data={!modalVisible ? hotels : ""}
+                                        horizontal={false}
+                                        showsHorizontalScrollIndicator={false}
+                                        numColumns={2}
+                                        renderItem={({ item }) => <ListImage hotel={item} />}
                                 />
-                        </View>   
-                        <Modal
-                     transparent={true}
-                     visible={modalVisible}
-                     onRequestClose={() => {
-                        setModalVisible(!modalVisible);
-                     }}
-                     >
-                        <View style={{alignItems:"center",marginTop:"50%"}}>
-                        <Image
-                                 source={{uri:modalVisible?itemzoom.image[index]:item.image[0]}}
-                                 style={{width:300,height:290,borderRadius:20}}
-                                /> 
                         </View>
-                     </Modal>                    
+                        <Modal
+                                transparent={true}
+                                visible={modalVisible}
+                                onRequestClose={() => {
+                                        setModalVisible(!modalVisible);
+                                }}
+                        >
+                                <View style={{ alignItems: "center", marginTop: "50%" }}>
+                                        <Image
+                                                source={{ uri: modalVisible ? itemzoom.image[index] : item.image[0] }}
+                                                style={{ width: 300, height: 290, borderRadius: 20 }}
+                                        />
+                                </View>
+                        </Modal>
                 </ScrollView>
         )
 }
