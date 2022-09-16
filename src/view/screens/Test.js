5c36446ab1467caf337bbfe8e63fc814f0735d51
nguyendinhduy13 <your_email@example.com>
2022-09-16 20:50:03 +0700

    clone

--- src/view/screens/Test.js
+++ src/view/screens/Test.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
 import firestore, { firebase } from '@react-native-firebase/firestore';
+import { createIconSetFromFontello } from 'react-native-vector-icons';
 export default function Test() {
     const [data, setData] = useState([])
     const [item, setItem] = useState(false)
@@ -29,8 +30,7 @@ export default function Test() {
             .get()
             .then(documentSnapshot => {
                 if (documentSnapshot.exists) {
-                    setData(a)
-                    setItem(!item)
+                    console.log(documentSnapshot.data())
                 }
             });
     }, [])
@@ -44,7 +44,7 @@ export default function Test() {
                 <Text>Log</Text>
             </TouchableOpacity>
             {item &&
-                console.log("aaaaaaaaa   ", a.age.id)
+                console.log("aaaaaaaaa   ")
             }
         </View>
     )
